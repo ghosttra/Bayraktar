97383a6b286636b2fa27d16d6a462cec2fde4281
tr1ckshxt <Skor_ws26@student.itstep.org>
2022-11-07 19:46:41 +0200

    attacker side 0.1v

---
+++ Bayraktar/AttackerWin.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BayraktarGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bayraktar {
+    /// <summary>
+    /// Interaction logic for AttackerWin.xaml
+    /// </summary>
+    public partial class AttackerWin : Window {
+
+        List<Unit> MilitaryUnits;
+        public AttackerWin() {
+            InitializeComponent();
+            List<Unit> MilitaryUnits = new List<Unit>();
+            MilitaryUnits.Add(new Unit() { Id = 0, CoolDown = 500, Name = "ArtZ", Price = 100 });
+            MilitaryUnits.Add(new Unit() { Id = 1, CoolDown = 400, Name = "BTR", Price = 400 });
+            MilitaryUnitsLists.ItemsSource = MilitaryUnits; //подвязать существующих юнитов в список
+
+            //MilitaryUnitsLists.Width = 550;
+            //for (int i = 0; i < 250; i++) {
+            //    MilitaryUnitsLists.Items.Add(i);
+            //}
+        }
+
+        private void AttackerMenu_MouseEnter(object sender, MouseEventArgs e) {
+            AttackerMenuCanvas.Margin = new Thickness(40,0,0,0);
+        }
+
+        private void AttackerMenu_MouseLeave(object sender, MouseEventArgs e) {
+            AttackerMenuCanvas.Margin = new Thickness(590, 0, 0, 0);
+        }
+    }
+}
